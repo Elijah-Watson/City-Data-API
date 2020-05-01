c09c174587bc861f8e97ec18df92f8e03a1c3f2a
Elijah Watson <ElijahLWatson@Gmail.com>
2020-05-01 05:03:18 -0400

    Configure db connection pool

--- src/utils.js
+++ src/utils.js
@@ -10,6 +10,12 @@ module.exports.createStore = () => {
 		password: process.env.DB_PASSWORD,
 		host: process.env.DB_HOST,
 		port: process.env.DB_PORT,
+		pool: {
+			max: 5,
+			min: 0,
+			acquire: 30000,
+			idle: 10000
+		},
 		define: {
 			timestamps: false
 		},
